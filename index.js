0fed675ea126611e835764317167141480a3a5e9
Verners <verners_sccp@hotmail.com>
2020-10-10 18:32:33 -0300

    Adicionado cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const consign = require("consign");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
